231b767c4711cb90c4f40d5877644605d1b5711c
Sawyer Brunn <sawyerbrunn@berkeley.edu>
2022-06-12 18:43:00 -0700

    Implemented basic tic tac toe UI and backend Board helpers

---
+++ lib/tic_tac_toe/board.ex
@@ -0,0 +1,110 @@
+defmodule LiveTest.TicTacToe.Board do
+  @moduledoc """
+  A simple representation of a Tic Tac Toe board.
+
+  A board has a state, a current turn, and a history.
+  """
+
+  @initial_state Enum.map(0..8, & {&1, nil}) |> Map.new()
+  @pieces ["X", "O"]
+
+  defstruct [
+    state: @initial_state,
+    current_turn: "X",
+    turn_number: 0,
+    history: %{}
+  ]
+
+  @doc """
+  Creates a fresh tic tac toe board, initialized
+  to the empty state.
+  """
+  def new() do
+    %__MODULE__{}
+  end
+
+  @doc """
+  Attempts to place the given piece at the given
+  index on the board.
+
+  Returns a new board is successful. Otherwise,
+  returns an atom representing the error.
+  """
+  def make_move(%__MODULE__{} = board, piece, index) do
+    case validate_move(board, piece, index) do
+      :ok ->
+        %__MODULE__{
+          state: Map.put(board.state, index, piece),
+          current_turn: other(board.current_turn),
+          turn_number: board.turn_number + 1,
+          history: Map.put(board.history, board.turn_number, board.state)
+        }
+      error ->
+        error
+    end
+  end
+
+  @doc """
+  Undoes the last move make on the board, and returns
+  a new board.
+
+  Returns :error if there is nothing to undo.
+  """
+  def undo(%__MODULE__{turn_number: turn_number} = board) do
+    if turn_number == 0 do
+      :error
+    else
+      %__MODULE__{
+        state: Map.get(board.history, turn_number - 1),
+        current_turn: other(board.current_turn),
+        turn_number: turn_number - 1,
+        history: Map.delete(board.history, turn_number - 1)
+      }
+    end
+  end
+
+  def validate_move(board, piece, index) do
+    cond do
+      find_winner(board) != nil ->
+        :game_over
+      board.current_turn != piece ->
+        :wrong_turn
+      piece not in @pieces ->
+        :invalid_piece
+      Map.get(board.state, index, :bad_index) != nil ->
+        :nonempty_square
+      true ->
+        :ok
+    end
+  end
+
+  def find_winner(board) do
+    state = board.state
+    checks = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+    Enum.map(checks, fn check_list ->
+      [i1, i2, i3] = check_list
+      if Map.get(state, i1) != nil and Map.get(state, i1) == Map.get(state, i2) and Map.get(state, i2) == Map.get(state, i3) do
+        Map.get(state, i1)
+      else
+        nil
+      end
+    end)
+    |> Enum.filter(& &1 != nil)
+    |> case do
+      [] -> nil
+      _ = winner_list -> Enum.at(winner_list, 0)
+    end
+  end
+
+  defp other("X"), do: "O"
+  defp other("O"), do: "X"
+end
