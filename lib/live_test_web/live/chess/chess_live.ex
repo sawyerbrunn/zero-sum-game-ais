ee973b386a96536412edf968bae71f4a59738b00
Sawyer Brunn <sawyerbrunn@berkeley.edu>
2022-06-21 23:34:10 -0700

    Add carat svg icons to AI dropdowns in chess UI

--- lib/live_test_web/live/chess/chess_live.ex
+++ lib/live_test_web/live/chess/chess_live.ex
@@ -164,9 +164,40 @@ defmodule LiveTestWeb.ChessLive do
     ~H"""
     <div class="grid grid-cols-3 space-x-1">
       <div class="col-start-1 col-span-1">
-        <button class="w-full inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out"
+        <button class="flex justify-center w-full inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out"
           phx-click="change-wso" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
           White AI
+          <%= if @wso do %>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="caret-up"
+              class="w-2 ml-2"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512">
+              <path
+                fill="currentColor"
+                d="M288.662 352H31.338c-17.818 0-26.741-21.543-14.142-34.142l128.662-128.662c7.81-7.81 20.474-7.81 28.284 0l128.662 128.662c12.6 12.599 3.676 34.142-14.142 34.142z">
+              </path>
+            </svg>
+          <% else %>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="caret-down"
+              class="w-2 ml-2"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512">
+              <path
+                fill="currentColor"
+                d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z">
+              </path>
+            </svg>
+          <% end %>
         </button>
       </div>
       <div class="col-start-2 col-span-1">
@@ -175,9 +206,40 @@ defmodule LiveTestWeb.ChessLive do
         </button>
       </div>
       <div class="col-start-3 col-span-1">
-        <button class="w-full inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out"
-        phx-click="change-bso" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            Black AI
+        <button class="flex justify-center w-full inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out"
+          phx-click="change-bso" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Black AI
+          <%= if @bso do %>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="caret-up"
+              class="w-2 ml-2"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512">
+              <path
+                fill="currentColor"
+                d="M288.662 352H31.338c-17.818 0-26.741-21.543-14.142-34.142l128.662-128.662c7.81-7.81 20.474-7.81 28.284 0l128.662 128.662c12.6 12.599 3.676 34.142-14.142 34.142z">
+              </path>
+            </svg>
+          <% else %>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="caret-down"
+              class="w-2 ml-2"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512">
+              <path
+                fill="currentColor"
+                d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z">
+              </path>
+            </svg>
+          <% end %>
         </button>
       </div>
     </div>
@@ -290,22 +352,22 @@ defmodule LiveTestWeb.ChessLive do
     ~H"""
     <div class="grid grid-cols-4 space-x-2">
       <div class="col-start-1 col-span-1">
-        <button data-bs-toggle="modal" data-bs-target="#newGameModal" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 border border-gray-700 rounded w-full">
+        <button data-bs-toggle="modal" data-bs-target="#newGameModal" class="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 border border-gray-700 rounded w-full">
           New
         </button>
       </div>
       <div class="col-start-2 col-span-1">
-        <button id="undoBtn" phx-click="pause-ai-game" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 border border-gray-700 rounded w-full">
+        <button id="undoBtn" phx-click="pause-ai-game" class="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 border border-gray-700 rounded w-full">
           Undo
         </button>
       </div>
       <div class="col-start-3 col-span-1">
-        <button id="flipBoardBtn" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 border border-gray-700 rounded w-full">
+        <button id="flipBoardBtn" class="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 border border-gray-700 rounded w-full">
           Flip
         </button>
       </div>
       <div class="col-start-4 col-span-1">
-        <button data-bs-toggle="modal" data-bs-target="#fenModal" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 border border-gray-700 rounded w-full">
+        <button data-bs-toggle="modal" data-bs-target="#fenModal" class="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 border border-gray-700 rounded w-full">
           Fen
         </button>
       </div>
